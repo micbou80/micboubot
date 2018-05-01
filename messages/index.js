5fcb302a4f6e0b113ab3d8abf426660b04b0c9d4
Michel Bouman <34888468+micbou80@users.noreply.github.com>
2018-05-01 19:00:51 +0200

    VideoHero

--- messages/index.js
+++ messages/index.js
@@ -472,15 +472,15 @@ bot.dialog('/inboxzero', [
 bot.dialog('/video', [
     (session, args, next) => {
         const card = new builder.VideoCard(session)
-            .title('Big Buck Bunny')
-            .subtitle('by the Blender Institute')
-            .text('Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation\'s previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.')
+            .title('The animated guide to artificial intelligence')
+            .subtitle('(Explanimators: Episode 1)')
+            .text('An easy guide to everything AI. More from Microsoft Story Labs: microsoft.com/storylabs.')
             .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg'))
             .media([
-                { url: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }
+                { url: 'https://www.youtube.com/watch?v=4NsilUpnRY0&t=2s' }
             ])
             .buttons([
-                builder.CardAction.openUrl(session, 'https://peach.blender.org/', 'Learn More')
+                builder.CardAction.openUrl(session, 'https://www.microsoft.com/en-us/ai/', 'Learn More')
             ]);
 
         const msg = new builder.Message(session).addAttachment(card);
